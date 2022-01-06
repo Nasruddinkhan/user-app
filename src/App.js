f93444d0abb802975bc1d10bcded97a941e01006
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-01-06 20:44:45 +0530

    open the modal in food app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./component/Layout/Header";
 import Meals from "./component/Meals/Meals";
-
+import Cart  from "./component/Cart/Cart"
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  }
+  const hindCartHandler = () => {
+    setCartIsShow(false);
+  }
   return (
     <Fragment>
-      <Header />
+      {cartIsShow && <Cart onClose={hindCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
