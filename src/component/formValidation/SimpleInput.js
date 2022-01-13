c1b27828700233be17a8aee56abdc6762e47fa15
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-01-14 00:44:15 +0530

    optimize the form validation

--- src/component/formValidation/SimpleInput.js
+++ src/component/formValidation/SimpleInput.js
@@ -1,70 +1,102 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from 'react';
 import Card from "../../UI/Card";
 import classes from "./SimpleInput.module.css";
 
 const SimpleInput = () => {
-  const nameInputRef = useRef();
-  const [enterName, setEnterName] = useState("");
-  const [enterNameIsValid, setEnterNameIsValid] = useState(false);
+  
+
+  const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  useEffect(() => {
-    if (enterNameIsValid) console.log("Name input is valid");
-  }, [enterNameIsValid]);
+
+   const [enteredEmail, setEnteredEmail] = useState("");
+   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+   const enteredEmailIsValid = enteredEmail.includes('@');
+   const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
-    setEnterName(event.target.value);
-     if (event.target.value.trim() !== "") {
-       setEnterNameIsValid(true);
-     }
+    setEnteredName(event.target.value);
   };
 
-    const nameInputBlurHandler = (event) => {
-      setEnterName(event.target.value);
-      setEnteredNameTouched(true);
-      if (enterName.trim() === '') {
-          setEnterNameIsValid(false)
-          return
-      }
-    };
- 
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (enterName.trim() === "") {
-      setEnterNameIsValid(false);
+  };
+  
+  
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
+  };
+  
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
+
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnterNameIsValid(true);
-    const enterValue = nameInputRef.current.value;
-    console.log(enterValue);
-    setEnterName("");
+
+    console.log(enteredName);
+
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredName('');
+    setEnteredNameTouched(false);
   };
-  const nameInputIsInvalid = !enterNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
-    ? "form-control invalid"
-    : "form-control";
+    ? 'form-control invalid'
+    : 'form-control';
+
+      const emailInputClasses = emailInputIsInvalid
+        ? "form-control invalid"
+        : "form-control";
 
   return (
     <Card cssClassName={classes.simpleform}>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={formSubmissionHandler}>
         <div className={nameInputClasses}>
           <label htmlFor="name">Name</label>
           <input
             type="text"
-            ref={nameInputRef}
-            value={enterName}
-            onChange={nameInputChangeHandler}
             id="name"
+            onChange={nameInputChangeHandler}
             onBlur={nameInputBlurHandler}
+            value={enteredName}
           />
           {nameInputIsInvalid && (
-            <p className="error-text"> Enter the name cannot be empty!</p>
+            <p className="error-text">Name must not be empty.</p>
           )}
         </div>
-
+        <div className={emailInputClasses}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="name"
+            onChange={emailInputChangeHandler}
+            onBlur={emailInputBlurHandler}
+            value={enteredEmail}
+          />
+          {emailInputIsInvalid && (
+            <p className="error-text">Email must not be empty.</p>
+          )}
+        </div>
         <div className="form-actions">
-          <button>Submit</button>
+          <button disabled={!formIsValid}>Submit</button>
         </div>
       </form>
     </Card>
