5c4892acc7802d7bed8ccadf8719273725a25886
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-01-16 19:04:41 +0530

    add the toggle hide show action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,33 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { Component } from "react";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+   const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+     dispatch({ type: "toggele" });
+  };
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
-
+  const   incrementBy10Handler = () => {
+    dispatch({ type: "incrementBy10", incrementBy:10 });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      {show && <div className={classes.value}>{counter}</div>}
+      {show && (
+        <div>
+          <button onClick={incrementBy10Handler}>Increment By 10</button>
+          <button onClick={incrementHandler}>Increment</button>
+          <button onClick={decrementHandler}>Decrement</button>
+        </div>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/CounterStore.js
+++ src/store/CounterStore.js
@@ -1,20 +1,34 @@
 import { createStore } from "redux";
 
+const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-      return {
-        counter: state.counter + 1
-      };
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
   if (action.type === "decrement") {
-       return {
-         counter: state.counter - 1,
-       };
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
   }
+  if (action.type === "toggele") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+   if (action.type === "incrementBy10") {
+     return {
+       counter: state.counter + action.incrementBy,
+       showCounter: state.showCounter,
+     };
+   }
   return state;
 };
-
 const store = createStore(counterReducer);
 
 export default store;
