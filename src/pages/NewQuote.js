5efe2ea6ec1409385b25a7ed96b01ee6e7a460f1
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-01-23 23:54:19 +0530

    add the quate details

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,11 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
+const NewQuote = () => {
+  const addQuoteHandler = (quoteData) =>{
+    console.table(quoteData);
+    use
+  }
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
+
+export default NewQuote;
