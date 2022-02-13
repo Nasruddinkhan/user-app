3379d65e6173cf0534d2121c301ae4527e8d8bf3
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-02-13 23:19:07 +0530

    add the transition

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,6 +24,44 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({
+              showBlock: !prevState.showBlock,
+            }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+          {state=> (
+            <p>
+              {
+                <div
+                  style={{
+                    margin: "auto",
+                    backgroundColor: "green",
+                    width: 100,
+                    height: 100,
+                    transition: 'opacity 1s ease-out',
+                    opacity: state === 'exiting' ? 0 : 1
+                  }}
+                ></div>
+              }
+            </p>
+          )}
+        </Transition>
+        {/* {this.state.showBlock ? (
+          <div
+            style={{
+              backgroundColor: "green",
+              width: 100,
+              height: 100,
+            }}
+          ></div>
+        ) : null} */}
         {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> */}
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
