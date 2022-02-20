5b4bf4f20604394fcc3d4bc8bd07ccae0efd7d6e
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-02-20 13:47:04 +0530

    add firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 
+const URL =
+  "https://my-second-project-50046-default-rtdb.firebaseio.com/ingredients.json";
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
-  const addIngredientsHandler = (ingredients) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredients },
-    ]);
+  const addIngredientsHandler = async (ingredients) => {
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ingredients),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredients },
+        ]);
+      });
   };
   const onRemoveItemHandler = (ingredientId) => {
     setUserIngredients((prevIngredients) =>
       prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
     );
   };
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   return (
     <div className="App">
