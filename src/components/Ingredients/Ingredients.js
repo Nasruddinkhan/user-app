b0cd6ca2c3892933cb9d0ca0269ce78677a2e74a
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-02-21 19:43:31 +0530

    add reducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,14 +1,33 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useReducer, useState } from "react";
 import { useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 import ErrorModal from "../UI/ErrorModal";
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter(
+        (ingredient) => ingredient.id !== action.id
+      );
+    default:
+      throw new Error("Should not go there");
+  }
+};
+
 const URL =
   "https://my-second-project-50046-default-rtdb.firebaseio.com/ingredients.json";
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+
+ const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+
+ // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -26,10 +45,10 @@ function Ingredients() {
         return response.json();
       })
       .then((responseData) => {
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredients },
-        ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredients },
+        });
       })
       .catch((error) => {
         setError("Something went wrong!");
@@ -40,7 +59,7 @@ function Ingredients() {
   const onRemoveItemHandler = (ingredientId) => {
     setIsLoading(true);
     fetch(
-      `https://my-second-project-50046-default-rtdb.firebaseio.com/ingredients/${ingredientId}.jon`,
+      `https://my-second-project-50046-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: "DELETE",
       }
@@ -48,9 +67,13 @@ function Ingredients() {
       .then((response) => {
         setIsLoading(false);
 
-        setUserIngredients((prevIngredients) =>
-          prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-        );
+        // setUserIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        // );
+        dispatch({
+          type: "DELETE",
+          id: ingredientId,
+        });
       })
       .catch((error) => {
         setError("Something went wrong!");
@@ -62,24 +85,24 @@ function Ingredients() {
     console.log("RENDERING INGREDIENTS", userIngredients);
   }, [userIngredients]);
 
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(URL)
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount,
+  //         });
+  //       }
+  //       setUserIngredients(loadedIngredients);
+  //     });
+  // }, []);
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   const clearError = () => {
